94054f7444dfd4f7585ea5ceec44027933f5ccd0
isarahmany <blackc1p3r@gmail.com>
2019-05-05 21:57:18 -0700

    Update app.js
    added routes for express.js and configured ssl support

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
 const sqrl = require('squirrelly');
 const app = require('express')();
+const fs = require('fs');
+const https = require('https');
+
+const privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
+const certificate = fs.readFileSync('ssl/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
 const httpPort = 80;
 const sslPort = 443;
+const baseURL = 'http://localhost/';
 const views = {
-    index: 'views\\index.sqrl',
+
+    splash: 'views\\slpash.sqrl',
+    projects: 'views\\projects.sqrl',
+    me: 'views\\me.sqrl',
+    contact: 'views\\contact.sqrl'
+
 };
 
+//Site Get Index / Serve Splash Page
 app.get('/', (req, res) => {
-    const rendered = sqrl.renderFile(views.index, {data: "Hello!"});
+    const rendered = sqrl.renderFile(views.splash);
     res.send(rendered);
 });
 
+//Serve Projects Page
+app.get('/projects', (req, res) => {
+    const rendered = sqrl.renderFile(views.projects, {baseURI: baseURL, pages: {"Projects" : "/projects", "Me" : "/me", "Contact" : "/contact"}
+    });
+    res.send(rendered);
+});
+
+//Serve Me Portfolio Page
+app.get('/me', (req, res) => {
+    const rendered = sqrl.renderFile(views.me, {baseURI: baseURL, pages: {"Projects" : "/projects", "Me" : "/me", "Contact" : "/contact"}
+    });
+    res.send(rendered);
+});
+
+//Serve Contact Page
+app.get('/contact', (req, res) => {
+    const rendered = sqrl.renderFile(views.contact, {baseURI: baseURL, pages: {"Projects" : "/projects", "Me" : "/me", "Contact" : "/contact"}
+    });
+    res.send(rendered);
+});
+
+//Listen on port 80 HTTP (NOSSL)
 app.listen(httpPort, () => console.log(`Node Server (NOSSL) listening on port ${httpPort}!`));
+
+//Listen on port 443 HTTPS (SSL) 
+
+/*
+
+//UNCOMMENT AND CONFIGURE TO ENABLE
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(sslPort, () => {console.log(`Node Server (SSL) listening on port ${sslPort}`)});
+
+*/
